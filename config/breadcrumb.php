662e5b6399ccc4b96b2a3eb9f2ac22b20958074a
kjmtrue <kimtrien@gmail.com>
2016-01-14 23:49:15 +0700

    Update fix

--- config/breadcrumb.php
+++ config/breadcrumb.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: NKT
- * Date: 27/12/2015
- * Time: 10:07
- */
-
 return [
     'divider'       => '<i class="fa fa-angle-right"></i>',
     'cssClass'      => ['page-breadcrumb'],
